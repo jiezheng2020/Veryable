05ccfa82879c0b904e3434a5e10858963344190b
Kevin Zheng <jiezheng2020@gmail.com>
2021-08-12 22:46:28 -0500

    navbar complete

--- my-app/src/components/NavBar/NavBar.js
+++ my-app/src/components/NavBar/NavBar.js
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
+import "./NavBar.css";
+import GroupsIcon from "../../icons/group.svg";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar_content">
+        <img src={GroupsIcon}></img>
+        <span>USERS</span>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
